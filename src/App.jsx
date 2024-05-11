3ad03368cd2e35fcb094b280435029ff7bc58408
zana-abdollahpour <zana.ap4@gmail.com>
2024-05-11 15:34:24 +0330

    data-source

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import "./App.css";
 
+import DataSource from "./components/data-source";
 import ResourceLoader from "./components/resource-loader";
 import { UserInfo } from "./components/user-info";
+
+const getDataFromServer = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 function App() {
   return (
@@ -9,6 +16,13 @@ function App() {
       <ResourceLoader resourceUrl="/users/2" resourceName="user">
         <UserInfo />
       </ResourceLoader>
+
+      <DataSource
+        resourceName="user"
+        getData={() => getDataFromServer("/users/3")}
+      >
+        <UserInfo />
+      </DataSource>
     </div>
   );
 }
---
+++ src/components/data-source.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+export default function DataSource({
+  getData = () => {},
+  resourceName,
+  children,
+}) {
+  const [resource, setResource] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getData();
+      setResource(data);
+    })();
+  }, [getData]);
+
+  return (
+    <>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child))
+          return React.cloneElement(child, { [resourceName]: resource });
+        return child;
+      })}
+    </>
+  );
+}
