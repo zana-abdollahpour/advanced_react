d813fea2c4e83edefe298f1cb70a8b1f7d7da0b2
zana-abdollahpour <zana.ap4@gmail.com>
2024-05-31 08:48:47 +0330

    add debounce

---
+++ src/helpers/debounce.js
@@ -0,0 +1,11 @@
+export function debounce(fn, delay) {
+  let timerId;
+
+  return (...args) => {
+    if (timerId) clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
